5addb1afdbc0d995d0162e9439ad9bc177c59516
Lukas Gruber <lgruber.mmt-m2019@fh-salzburg.ac.at>
2020-11-22 18:16:00 +0100

    doc: add doc

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -21,6 +21,11 @@ var (
 	shoppingSearch = fakeSearch("Shopping")
 )
 
+// firstResult ... sends requests to all replicas and returns only the fastest one
+func firstResult(query string, replicas ...search) []Result {
+	// todo
+}
+
 // LoadResults - sends requests to different search backends and aggregates the result.
 func LoadResults(query string) []Result {
 	var results []Result
@@ -32,6 +37,19 @@ func LoadResults(query string) []Result {
 		shoppingSearch,
 	}
 	c := make(chan []Result)
+
+	/*
+		todo: Add multiple replicas for every search backend.
+		Send requests to all of them and use only the fastest result.
+
+		You will need:
+		- func, closures
+		- chan []Result
+		- Goroutines
+
+		hint: Fan-In pattern
+		https://github.com/imba28/mmt-go-snippets/blob/master/3-concurrency/3-select/main.go#L17
+	*/
 
 	for i := 0; i < len(backends); i++ {
 		go func(backend search) {
